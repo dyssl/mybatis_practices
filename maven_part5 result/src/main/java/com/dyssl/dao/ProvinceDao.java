cc21f76d2ddb7a985757e00cd74a89ff00bc00a3
dyssl <1131838595@qq.com>
2022-03-29 10:54:45 +0800

    delete

--- maven_part5 result/src/main/java/com/dyssl/dao/ProvinceDao.java
+++
@@ -1,9 +0,0 @@
-package com.dyssl.dao;
-
-import com.vo.ProvinceCity;
-
-import java.util.List;
-
-public interface ProvinceDao {
-    List<ProvinceCity> selectProvinceCityList(Integer provinceid);
-}
